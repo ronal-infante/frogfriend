b27211993e6996039b68f8c1fafc4df6fbccedf7
Ronal Infante <ronal.infante@gmail.com>
2021-11-08 02:56:16 -0600

    Dump Miro /dist

---
+++ assets/app.5d9db116.js
@@ -0,0 +1,1 @@
+/* empty css              */miro.onReady((async function(){const[o]=await miro.board.widgets.create({type:"sticker",text:"Hello, World!"});await miro.board.viewport.zoomToObject(o)}));
