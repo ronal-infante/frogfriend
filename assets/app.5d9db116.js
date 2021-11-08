309ae1a353b9e8c75c502acf2bc82d8a7be3a89c
Ronal Infante <ronal.infante@gmail.com>
2021-11-08 03:03:23 -0600

    Edit to /dist

--- assets/app.5d9db116.js
+++ assets/app.5d9db116.js
@@ -1,1 +1,8 @@
-/* empty css              */miro.onReady((async function(){const[o]=await miro.board.widgets.create({type:"sticker",text:"Hello, World!"});await miro.board.viewport.zoomToObject(o)}));
+/* empty css              */
+miro.onReady((async function() {
+    const [o] = await miro.board.widgets.create({
+        type: "sticker",
+        text: "Yay!"
+    });
+    await miro.board.viewport.zoomToObject(o)
+}));
